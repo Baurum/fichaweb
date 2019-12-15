1160ca3b1596b273c7ba6eb792b33aae02b6073c
Marcos Bau <marcosbau5@gmail.com>
2019-12-15 13:03:40 +0100

    add filename

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__filename + '/dist/fichaweb'));
+app.use(express.static(__dirname + '/dist/fichaweb'));
 
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__filename + '/dist/fichaweb/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/fichaweb/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000);
